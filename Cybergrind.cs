346d0b6afdd6f95818b9d278b007854a995923f4
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2022-12-11 15:50:50 -0600

    guh
    added try catch exception + new config

--- Cybergrind.cs
+++ Cybergrind.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 using UMM;
 using UnityEngine.SceneManagement;
@@ -6,6 +6,9 @@ using HarmonyLib;
 using BepInEx;
 using BepInEx.Configuration;
 using GameConsole;
+using UnityEngine;
+using System;
+using JetBrains.Annotations;
 
 namespace UltraRunTracker
 {
@@ -14,34 +17,42 @@ namespace UltraRunTracker
     public class CyberGame : UKMod
     {
         MainGame mg = new MainGame();
-        
 
-        readonly string dirMG = Directory.GetCurrentDirectory() + @"\BepInEx\UMM Mods\UltraRunTracker\runStats.csv";
-        readonly string dir = Directory.GetCurrentDirectory() + @"\BepInEx\UMM Mods\UltraRunTracker\cyberStats.csv";
+        DateTime Time = DateTime.Now;
+
+
+        public ConfigFile Config = new ConfigFile(Path.Combine(Paths.ConfigPath, "UltraRunTracker" + ".cfg"), false);
+
+        public ConfigEntry<bool> SeperateLevels;
+
+        public ConfigEntry<bool> seperateSessions;
+
+        string dir = Directory.GetCurrentDirectory() + @"\BepInEx\UMM Mods\UltraRunTracker\cyberStats.csv";
 
         private List<string> stats = new List<string>();
 
         bool loggedStats = false;
+        public static string session = "test"; 
 
         public void Awake()
         {
             Harmony harmony = new Harmony("UltraRunTracker");
             harmony.PatchAll();
 
-            mg.SeperateLevels = mg.Config.Bind("General", "SeperateLevels", false, "whether  or not to seperate each level into it's own file. ");
+            SeperateLevels = Config.Bind("Toggles", "SeperateLevels", false, "whether  or not to seperate each level into it's own file. ");
 
-            if (!File.Exists(dirMG))
+            seperateSessions = Config.Bind("Toggles", "SeperateSessions", false, "whether  or not to seperate each Session (every time you launch the game) into it's own file. ");
+
+            if (seperateSessions.Value) 
             {
-                File.AppendAllText(dirMG, "FinalRank,TimeRank,KillRank,StyleRank,Level,Challange,TookDamage,Kills,Seconds,Stylepoints,Restarts,Difficulty,Cheats,MajorAssists\n");
-            }
-            if (!File.Exists(dir))
-            {
-                File.AppendAllText(dir, "Wave,Kills,Seconds,Style,Difficulty,Cheats,MajorAssists\n");
-            }
+                session = Time.ToString("MM'.'dd'.'HH'.'mm'.'ss");
+                Debug.Log(session);
+            }       
             SceneManager.sceneLoaded += OnSceneLoaded;
 
         }
         private void AddStats() {
+
             stats.Add(EndlessGrid.Instance.currentWave.ToString());
             stats.Add(StatsManager.Instance.kills.ToString());
             stats.Add(StatsManager.Instance.seconds.ToString());
@@ -49,7 +60,8 @@ namespace UltraRunTracker
             stats.Add(PrefsManager.Instance.GetInt("difficulty").ToString());
             stats.Add(CheatsController.Instance.cheatsEnabled.ToString());
             stats.Add(AssistController.Instance.majorEnabled.ToString());
-            loggedStats = true;        
+            loggedStats = true;   
+            
         }
 
         void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
@@ -61,12 +73,29 @@ namespace UltraRunTracker
         public void Update() {
             if (SceneManager.GetActiveScene().name == "Endless" && NewMovement.Instance.dead && !loggedStats) // finalcyberrank.gameover is private :cryge:
             {
-                AddStats();
-                for (int i = 0; i < stats.Count; i++)
+                try
                 {
-                    File.AppendAllText(dir, stats[i]);
-                    if (i != stats.Count - 1) { File.AppendAllText(dir, ", "); }
-                    else { File.AppendAllText(dir, "\n"); }
+                    if (seperateSessions.Value)
+                    {
+                        dir = Directory.GetCurrentDirectory() + @"\BepInEx\UMM Mods\UltraRunTracker\cyberStats - " + session + ".csv";
+                    }
+
+                    if (!File.Exists(dir))
+                    {
+                        File.AppendAllText(dir, "Wave,Kills,Seconds,Style,Difficulty,Cheats,MajorAssists\n");
+                    }
+
+                    AddStats();
+                    for (int i = 0; i < stats.Count; i++)
+                    {
+                        File.AppendAllText(dir, stats[i]);
+                        if (i != stats.Count - 1) { File.AppendAllText(dir, ", "); }
+                        else { File.AppendAllText(dir, "\n"); }
+                    }
+                }
+                catch (Exception exception) {
+                    System.Console.WriteLine(exception);
+                    HudMessageReceiver.Instance.SendHudMessage("something went wrong, mostly likely\nyou have this level's <color=#ff0000ff>FILE</color> open, this run will <color=#ff0000ff>not</color> save."); // thanks Pitr
                 }
             }          
         }
